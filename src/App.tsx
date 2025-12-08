41a391f1ec05f23040c897a0649b4b8692921bdd
蓮華丸 <167959774+Rengemaru@users.noreply.github.com>
2025-12-08 23:55:38 +0900

    APIのデータを画面に反映

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,9 @@ function App() {
   ]
   // const [変数, 状態を管理する変数] = useState("初期値")と書くと状態を管理できるhookを使えるようになる → onclick(クリック属性)、onChange(リアルタイム変更)、onkeydown(キー入力受け付け)で使用できる
   const [keyword, setKeyword] = useState("");
-  // asyncで非同期処理の関数にできる
+  const [movieList, setMovieList] = useState([]);
+  // asyncで非同期処理にできる → awaitをつけずにfetchをつけるとfetchに時間がかかった場合すぐ次の処理(jsonに直す)に行ってしまうためデータが帰ってきてない状態で次に行ってしまう → つじつまが合わなくなってしまう
+  // awaitを使うことでデータのレスポンスが帰ってきてから次の処理に行くようにできる
   const fetchMovieList = async() => {
     const response = await fetch(
       "https://api.themoviedb.org/3/tv/popular?language=ja&page=1",
@@ -42,13 +44,17 @@ function App() {
         },
       }
     );
-
+    // jsonも上(await fetch)と同じ理由で時間がかかることがあるため await で待ってあげて、レスポンスが帰ってきてから次の処理に行くようにしてあげる
     const data = await response.json();
     // こう書くことで持ってきたデータをjsonとして扱うことができる
-    console.log(data)
+    setMovieList(data.results);
+    // APIの方で results の中にデータが格納されているため、キャッシュは results
   }
-
+  // useEffect：画面が表示される前に実行されるもの → 空括弧を入れている理由がそれ
+  // 第二引数：依存配列
+  // 実はuseEffectでデータ取得はしないほうがいい → React QueryやSWRなどのライブラリを利用してデータを取得する
   useEffect(() => {
+    // 処理
     fetchMovieList()
   }, []);
 
@@ -63,13 +69,13 @@ function App() {
             filter(条件) → trueのものは表示、falseのものは非表示にできる
             filterの条件にincludesを使用することでその内容が含まれているかという条件にすることができる
             ※この場合はmovie.nameにkeywordが含まれているかという条件になる */}
-        {defaultMovieList
-        .filter((movie) => movie.name.includes(keyword))
+        {movieList
+        .filter((movie) => movie.original_name.includes(keyword))
         .map((movie) => (
           <div key={movie.id}>
             {/* keyをつけることによって効率的に、より整合性を持って画面の更新ができる　←　つけないといけないんだなーと思えばいい */}
-            <h2>{movie.name}</h2>
-            <img src={movie.image} />
+            <h2>{movie.original_name}</h2>
+            <img src={`https://media.themoviedb.org/t/p/w300_and_h450_face/${movie.poster_path}`} />
             <p>{movie.overview}</p>
           </div>
         ))}
