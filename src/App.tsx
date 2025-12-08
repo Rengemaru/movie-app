d86387a7cd002985bc10dd19be846f3b7e6e1366
蓮華丸 <167959774+Rengemaru@users.noreply.github.com>
2025-12-09 00:15:39 +0900

    typeを導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,38 @@
 import './App.css'
 import { useEffect, useState } from "react";
 
+// このアプリで使用するtype
+type Movie = {
+  id: number,
+  name: string,
+  image: string,
+  overview: string,
+}
+
+// APIのデータの中で定義されているtype
+type MovieJson = {
+  adult: boolean,
+  backdrop_path: string,
+  genre_ids: number[],
+  id: number,
+  original_language: string,
+  original_name: string,
+  overview: string,
+  popularity: number,
+  poster_path: string,
+  first_air_date: string,
+  name: string,
+  vote_average: number,
+  vote_count: number
+}
+
 function App() {
   // TypeScript
-  const defaultMovieList = [
-    {
-      id: 1,
-      name: "君の名は。",
-      image: "https://media.themoviedb.org/t/p/w300_and_h450_face/yLglTwyFOUZt5fNKm0PWL1PK5gm.jpg",
-      overview: "1,000年に1度のすい星来訪が、1か月後に迫る日本。山々に囲まれた田舎町に住む女子高生の三葉は、町長である父の選挙運動や、家系の神社の風習などに鬱屈（うっくつ）していた。それゆえに都会への憧れを強く持っていたが、ある日彼女は自分が都会に暮らしている少年になった夢を見る。夢では東京での生活を楽しみながらも、その不思議な感覚に困惑する三葉。一方、東京在住の男子高校生・瀧も自分が田舎町に生活する少女になった夢を見る。やがて、その奇妙な夢を通じて彼らは引き合うようになっていくが……。",
-    },
-    {
-      id: 2,
-      name: "ハウルの動く城",
-      image: "https://media.themoviedb.org/t/p/w300_and_h450_face/v0K2e1t6ocUNnkZ9BeiFdcOT9LG.jpg",
-      overview: "父親の帽子店で帽子を作って暮らしていた18歳のソフィーは、荒野の魔女の呪いで90歳の老婆の姿になってしまう。彼女はハンサムだが気弱な魔法使いハウルと出会って、彼の居城でいっしょに暮らすようになるが、その城は4本足で歩く動く城だった。",
-    },
-    {
-      id: 3,
-      name: "もののけ姫",
-      image: "https://media.themoviedb.org/t/p/w300_and_h450_face/mVdz3vlmioKWZaHTGfu99zIuayZ.jpg",
-      overview: "山里に住む若者アシタカは、怒りと憎しみにより“タタリ神”と化した猪神から呪いをかけられてしまう。呪いを解く術を求めて旅に出るアシタカはやがて、西方の地で“タタラ”の村にたどり着く。エボシ御前が率いるその村では、鉄を造り続けていたが、同時にそれは神々の住む森を破壊することでもあった。そして、そんなタタラ達に戦いを挑むサンの存在をアシタカは知る。人の子でありながら山犬に育てられた彼女は“もののけ姫”と呼ばれていた。",
-    },
-    {
-      id: 4,
-      name: "バックトゥザフューチャー",
-      image: "https://media.themoviedb.org/t/p/w300_and_h450_face/oHaxzQXWSvIsctZfAYSW0tn54gQ.jpg",
-      overview: "スティーブン・スピルバーグとロバート・ゼメキスが贈るSFアドベンチャーシリーズ第1弾。高校生のマーティは、科学者・ドクの発明したタイムマシン・デロリアンで過去にタイムスリップしてしまう。",
-    }
-  ]
   // const [変数, 状態を管理する変数] = useState("初期値")と書くと状態を管理できるhookを使えるようになる → onclick(クリック属性)、onChange(リアルタイム変更)、onkeydown(キー入力受け付け)で使用できる
   const [keyword, setKeyword] = useState("");
-  const [movieList, setMovieList] = useState([]);
+  // <Movie[]>と置くことで、Movieのtype以外が参照できないようにする
+  const [movieList, setMovieList] = useState<Movie[]>([]);
+
   // asyncで非同期処理にできる → awaitをつけずにfetchをつけるとfetchに時間がかかった場合すぐ次の処理(jsonに直す)に行ってしまうためデータが帰ってきてない状態で次に行ってしまう → つじつまが合わなくなってしまう
   // awaitを使うことでデータのレスポンスが帰ってきてから次の処理に行くようにできる
   const fetchMovieList = async() => {
@@ -47,7 +48,13 @@ function App() {
     // jsonも上(await fetch)と同じ理由で時間がかかることがあるため await で待ってあげて、レスポンスが帰ってきてから次の処理に行くようにしてあげる
     const data = await response.json();
     // こう書くことで持ってきたデータをjsonとして扱うことができる
-    setMovieList(data.results);
+    // Jsonに直すときに、MovieとMovieJsonの型をすり合わせる
+    setMovieList(data.results.map((movie: MovieJson) => ({
+      id: movie.id,
+      name: movie.original_name,
+      image: movie.poster_path,
+      overview: movie.overview,
+    })));
     // APIの方で results の中にデータが格納されているため、キャッシュは results
   }
   // useEffect：画面が表示される前に実行されるもの → 空括弧を入れている理由がそれ
@@ -70,12 +77,12 @@ function App() {
             filterの条件にincludesを使用することでその内容が含まれているかという条件にすることができる
             ※この場合はmovie.nameにkeywordが含まれているかという条件になる */}
         {movieList
-        .filter((movie) => movie.original_name.includes(keyword))
+        .filter((movie) => movie.name.includes(keyword))
         .map((movie) => (
           <div key={movie.id}>
             {/* keyをつけることによって効率的に、より整合性を持って画面の更新ができる　←　つけないといけないんだなーと思えばいい */}
-            <h2>{movie.original_name}</h2>
-            <img src={`https://media.themoviedb.org/t/p/w300_and_h450_face/${movie.poster_path}`} />
+            <h2>{movie.name}</h2>
+            <img src={`https://media.themoviedb.org/t/p/w300_and_h450_face/${movie.image}`} />
             <p>{movie.overview}</p>
           </div>
         ))}
