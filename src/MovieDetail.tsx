4a9e3a519c9644c28cf4840e86811d6784d9ab77
蓮華丸 <167959774+Rengemaru@users.noreply.github.com>
2025-12-10 00:43:29 +0900

    詳細ページ

--- src/MovieDetail.tsx
+++ src/MovieDetail.tsx
@@ -1,5 +1,17 @@
-import { useParams } from "react-router";
 import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+
+type Movie = {
+  id: string;
+  original_title: string;
+  poster_path: string;
+  overview: string;
+  year: number;
+  rating: number;
+  runtime: number;
+  score: number;
+  genres: string[];
+};
 
 type MovieDetailJson = {
   adult: boolean;
@@ -40,69 +52,57 @@ type MovieDetailJson = {
   video: boolean;
   vote_average: number;
   vote_count: number;
-}
+};
 
-type Movie = {
-  id: string,
-  original_title: string,
-  overview: string,
-  poster_path: string,
-  year: number,
-  rating: number,
-  runtime: number,
-  score: number,
-  genres: string[],
-}
-
-function MovieDetail(){
-  // useParams
-  const { id } = useParams();
+function MovieDetail() {
+  const { movieId } = useParams();
   const [movie, setMovie] = useState<Movie | null>(null);
+  useEffect(() => {
+    fetchMovieDetail();
+  }, []);
 
   const fetchMovieDetail = async () => {
+    const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
     const response = await fetch(
-      `https://api.themoviedb.org/3/movie/${id}?language=ja`,
+      `https://api.themoviedb.org/3/movie/${movieId}?language=ja&page=1&append_to_response=credits`,
       {
         headers: {
-          Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`,
+          Authorization: `Bearer ${API_KEY}`,
         },
       }
     );
-
     const data = (await response.json()) as MovieDetailJson;
     setMovie({
-          id: data.id,
-          original_title: data.original_title,
-          overview: data.overview,
-          poster_path: data.poster_path,
-           year: Number(data.release_date.split("-")[0]),
-          rating: data.vote_average,
-          runtime: data.runtime,
-          score: data.vote_count,
-          genres: data.genres.map((genre: { id: number; name: string}) => genre.name),
-        });
+      id: data.id,
+      original_title: data.title,
+      poster_path: data.poster_path,
+      year: Number(data.release_date.split("-")[0]),
+      rating: data.vote_average,
+      runtime: data.runtime,
+      score: data.vote_count,
+      overview: data.overview,
+      genres: data.genres.map((genre) => genre.name),
+    });
   };
-
-  useEffect(() => {
-    fetchMovieDetail();
-  }, [])
-
-  return(
+  return (
     <div>
       {movie && (
-      <div>
-        <h2>{movie.original_title}</h2>
-        <img src={`https://media.themoviedb.org/t/p/w300_and_h450_face/${movie.poster_path}`} />
-        <p>{movie.overview}</p>
-        <p>{movie.year}</p>
-        <p>{movie.rating}</p>
-        <p>{movie.runtime}</p>
-        <p>{movie.score}</p>
-        <p>{movie.genres}</p>
-      </div>
+        <div>
+          <h2>{movie.original_title}</h2>
+          <img
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+            alt={movie.original_title}
+          />
+          <p>{movie.overview}</p>
+          <p>{movie.year}</p>
+          <p>{movie.rating}</p>
+          <p>{movie.runtime}</p>
+          <p>{movie.score}</p>
+          <p>{movie.genres}</p>
+        </div>
       )}
     </div>
-  )
+  );
 }
 
 export default MovieDetail;
